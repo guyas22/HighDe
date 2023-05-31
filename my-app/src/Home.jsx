5d6ef551c04f420217f4af1f94f360e9bc0a1895
Guy Asulin <guyas22@gmail.com>
2023-05-31 15:54:50 +0300

    noa

---
+++ my-app/src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to HighDe!</h1>
+      <p>HighDe is a unique application that creates playlists based on the tempo of your Spotify songs.</p>
+      <p>Our goal is to make your music listening experience more enjoyable by perfectly matching your mood or activity with the tempo of the songs.</p>
+      <p>Get ready to explore a whole new dimension of music!</p>
+      <p>Log in with Spotify</p>
+    </div>
+  );
+}
+
+export default Home;
