beabfb48f3a5e30afeabd7a5df99a1f7ed5e867d
Guy Asulin <guyas22@gmail.com>
2023-06-03 18:45:26 +0300

    noaanthony with style

--- highde/src/components/SelectableTrack.jsx
+++ highde/src/components/SelectableTrack.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-
 const SelectableTrack = ({ track, isSelected, onSelect }) => {
-  const handleSelect = () => {
+  const handleClick = () => {
+    console.log(`Track clicked: ${track.name}`);  // Add this line
     onSelect(track);
   };
 
+  const imageUrl = track.album.images[0].url;
+  
   return (
-    <div>
-      <input type="checkbox" checked={isSelected} onChange={handleSelect} />
-      {track.name}
+    <div 
+      className={`track-card ${isSelected ? 'selected' : ''}`}
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      onClick={handleClick}
+    >
+      <h2>{track.name}</h2>
+      <p>{track.artists[0].name}</p>
     </div>
   );
 };
--- highde/src/components/TopTracks.jsx
+++ highde/src/components/TopTracks.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {getTrackFeatures, getTopTracks, createPlaylist, addTracksToPlaylist, getRecommendations } from '../api/spotify';
 import SelectableTrack from './SelectableTrack';
-
+import './TopTracks.css';
 const TopTracks = ({ token, userId }) => {
   const [tracks, setTracks] = useState([]);
   const [selectedTracks, setSelectedTracks] = useState([]);
@@ -17,14 +17,26 @@ const TopTracks = ({ token, userId }) => {
   }, [token]);
 
   const handleSelectTrack = (track) => {
+    console.log(`Track clicked: ${track.name}`);  // Add this line
     setSelectedTracks(prevTracks => {
+      // If the track is already selected, deselect it
       if (prevTracks.includes(track)) {
         return prevTracks.filter(t => t !== track);
       } else {
-        return [...prevTracks, track];
+        // If there are already 5 tracks selected, do nothing
+        if (prevTracks.length >= 5) {
+          console.warn("Maximum of 5 tracks can be selected");
+          return prevTracks;
+        }
+        // Otherwise, add the new track to the selection
+        else {
+          return [...prevTracks, track];
+        }
       }
     });
   };
+  
+
 
 
   
@@ -50,7 +62,7 @@ const TopTracks = ({ token, userId }) => {
 
   const handleGeneratePlaylist = async () => {
     if (selectedTracks.length !== 5) {
-      console.error("You must select exactly 10 tracks");
+      console.error("You must select exactly 5 tracks");
       return;
     }
 
@@ -83,21 +95,27 @@ const TopTracks = ({ token, userId }) => {
   };
 
   return (
-    <div>
+    <div className='top-tracks-container'>
       <h1>Top Tracks</h1>
-      {tracks.map(track => (
-        <SelectableTrack
-          key={track.id}
-          track={track}
-          isSelected={selectedTracks.includes(track)}
-          onSelect={handleSelectTrack}
-        />
-      ))}
-      <button disabled={selectedTracks.length !== 5} onClick={handleGeneratePlaylist}>
-        Generate Playlist
-      </button>
+      <div className='tracks-grid'>
+        {tracks.map(track => (
+          <SelectableTrack
+            key={track.id}
+            track={track}
+            isSelected={selectedTracks.includes(track)}
+            onSelect={handleSelectTrack}
+          />
+        ))}
+      </div>
+      <div className='generate-playlist-button'>
+        <button disabled={selectedTracks.length !== 5} onClick={handleGeneratePlaylist}>
+          Generate Playlist
+        </button>
+      </div>
     </div>
   );
+
+
 };
 
 export default TopTracks;
